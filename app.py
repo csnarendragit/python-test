24f158267a49e56009fb8e365afe7542329b7f11
csnarendragit <80242991+csnarendragit@users.noreply.github.com>
2023-05-21 18:12:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ server = Flask(__name__)
 
 @server.route("/")
 def welcome():
-    return "Welcome to devopswithcloud training - tommorrow"
+    return "Welcome to devopswithcloud training --- tommorrow"
 
 @server.route('/openshift')
 def hello():
